b1c10c48cd1d0c1fcd9fd6f4e9887d9e0c39f240
farihah31 <35025832+farihah31@users.noreply.github.com>
2019-08-22 14:43:06 +0800

    Add files via upload
    coding version 1

---
+++ hello_java.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author anis
+ */
+public class hello_java {
+ //hello_java adalah nama kelas ini
+    //class adalah nama kelas ini
+    
+        public static void main(String[]args){
+        //static adalah berfungsi untuk mengelak dari capaian pembolehubah
+        //void bermaksud tiada nilai dikembalikan
+                System.out.println("Selamat Datang 1SVM KPD!");
+                //out adalah merupakan untuk memaparkan output standard yang ditulis
+    }
+}
